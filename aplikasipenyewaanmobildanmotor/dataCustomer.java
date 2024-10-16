3ef9938f8d76480eacb227d443d31472bc5a3515
stanlymanik <2381007@unai.edu>
2024-10-16 22:10:33 +0700

    menambah class dataCustomer

---
+++ aplikasipenyewaanmobildanmotor/dataCustomer.java
@@ -0,0 +1,8 @@
+package aplikasipenyewaanmobildanmotor;
+
+public class dataCustomer {
+    public String nama;
+    public int indexBarisData = -1;
+    public boolean statusPembayaran = false;
+    public boolean statusPengembalian = false;
+}
