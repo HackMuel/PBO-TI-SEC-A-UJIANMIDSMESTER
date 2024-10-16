4fff8fa7f011ff8901a7eb823e8e30bfd0d153cd
rioohoo12 <2381036@unai.edu>
2024-10-16 21:29:12 +0700

    menyelesaikan method tambah kendaraan, method hapus data kendaraan, menu edit data kendaraan, class dataKendaraan

---
+++ aplikasipenyewaanmobildanmotor/admin.java
@@ -0,0 +1,32 @@
+package aplikasipenyewaanmobildanmotor;
+
+public class admin {
+    private static void menuTambahDataKendaraan(){
+        dataKendaraan dataBaru = new dataKendaraan();
+        masukanDataKendaraan(dataBaru);
+        kendaraan.add(dataBaru);
+    }
+
+    private static void menuHapusDataKendaraan(){
+        if (!apakahDataKosong()){
+            tampilkanDataKendaraan();
+            System.out.print("Masukan Baris Data Kendaraan Yang Ingin Di Hapus : ");
+            int indexData = input.nextInt() -1;
+            hapusDataKendaraan(indexData);
+        }
+    }
+
+    private static void menuEditDataKendaraan(){
+        if (!apakahDataKosong()){
+            dataKendaraan dataBaru = new dataKendaraan();
+            tampilkanDataKendaraan();
+            System.out.print("Masukan Baris Data Kendaraan Yang Ingin Di Edit : ");
+            int indexData = input.nextInt() -1;
+            if (apakahBarisTersedia(indexData)){
+                masukanDataKendaraan(dataBaru);
+                kendaraan.set(indexData,dataBaru);
+            }
+        }
+    }
+
+}
---
+++ aplikasipenyewaanmobildanmotor/dataKendaraan.java
@@ -0,0 +1,9 @@
+package aplikasipenyewaanmobildanmotor;
+
+public class dataKendaraan {
+    public String nama;
+    public String nomorPlat;
+    public boolean apakahMotor = false;
+    public boolean statusPenyewaan = false;
+    public int harga;
+}
